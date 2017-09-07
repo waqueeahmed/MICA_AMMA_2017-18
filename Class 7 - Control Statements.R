95db2bf1bdbf3a70c0bdee9f92f9dd2e53882f23
waqueeahmed <waquee16@micamail.in>
2017-09-07 23:40:43 +0530

    AMMA -Class Exercises

--- Class 7 - Control Statements.R
+++ Class 7 - Control Statements.R
@@ -72,7 +72,7 @@ a <- c(rbinom(10,4,0.5))
 ifelse(a>2,"Yes","No")
 # replace negative spend value with 0
 # set up library
-setwd("C:\\Ram\\R for Data Science\\data")
+setwd("C:\YYYYYY\AMMA 2017\DATA\data_2017")
 # read data
 prd_spend <-read.csv(file="prod_spend.csv",
                      head=T)
